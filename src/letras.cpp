761ed78fb4f82807008ed7e132c0d2da05754765
robertohueso <robertohueso96@gmail.com>
2016-01-21 01:15:56 +0100

    Checkpoint!

--- src/letras.cpp
+++ src/letras.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main(int argc, char *argv[]){
 	ifstream lectura_diccionario(argv[1]);
 	ifstream lectura_puntos(argv[2]);
+	Diccionario diccionario;
 	ConjuntoLetras letras;
 
-	lectura_diccionario >> letras;
+	lectura_diccionario >> diccionario;
+	diccionario >> letras;
 	letras.leerPuntos(lectura_puntos);
 
-  
+
 
 	return 0;
 }
