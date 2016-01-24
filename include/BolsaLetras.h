d9f6aae6cae61d371ba4430932b433b6746d3836
robertohueso <robertohueso96@gmail.com>
2016-01-24 02:05:09 +0100

    Documenta parcialmente BolsaLetras

--- include/BolsaLetras.h
+++ include/BolsaLetras.h
@@ -1,14 +1,36 @@
 #include <vector>
 #include "ConjuntoLetras.h"
 
+//FIXME Documentar TDA
 class BolsaLetras{
 private:
+  /**
+    *@brief Vector de char que contiene las letras aleatorias
+  */
   vector<char> letras;
 
 public:
+  /**
+		@brief Construye una bolsa de letras
+    *Almacena en el vector cada letra con sus repeticiones
+    *
+    *@param conjunto Conjunto de letras del que se tomarán los datos
+	**/
   BolsaLetras(const ConjuntoLetras &conjunto);
 
+  /**
+		@brief Devuelve una letra obtenida de manera aleatoria del vector
+    * y marca esa letra como "\0" en el vector
+    *
+    *@return Char con el caracter de la letra
+	**/
   char getLetra();
 
+  /**
+		@brief Obtiene una cantidad de letras extraidas de manera aleatoria
+    *
+    *@param cantidad Cantidad de letras que se desea extraer
+    *@return vector<char> con las letras extraidas
+	**/
   vector<char> getVectorLetras(unsigned int cantidad);
 };
